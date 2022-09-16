876de947214c834ac17c6e7199fc2371d85744a8
georgy <ganus1111@gmail.com>
2022-09-16 15:38:10 +0300

    add task solution

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,21 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        for (int i = 1; i <= cathetusLength; i++) {
+            int numberToPrint = cathetusLength;
+            for (int k = cathetusLength; k > i; k--) {
+                System.out.print(" ");
+                numberToPrint--;
+            }
+            for (int j = 1; j <= i; j++){
 
+                System.out.print(numberToPrint--);
+            }
+            for (int p = 1; p < i; p++){
+                System.out.print(numberToPrint++ + 2);
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
